31a978ee2c1a7b28dd2b36bc1aba24265a1393f0
ShellFolder <kk1593@gmail.com>
2023-04-01 20:01:06 +0900

    DevMovieVue Files

---
+++ DevMovieVue/vue-app/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <HeadView />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import HeadView from '@/components/HeadView.vue'
+export default {
+  name: 'App',
+  components: {
+    HeadView
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ DevMovieVue/vue-app/src/components/MovieView.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'MovieView'
+}
+</script>
+
+<style lang="scss">
+
+</style>
